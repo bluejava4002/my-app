1036530081e8a06ab21e878c660b6e187d3b8849
Marvin Galeski <marvin.galeski@gmail.com>
2026-03-09 14:15:28 +0100

    feat: initial commit

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <main className="p-10">
+      <h1 className="text-3xl font-bold mb-4 text-blue-500 bg-yellow-100 text-center">À propos</h1>
+      <p className="text-gray-600">
+      <p className="text-gray-600">
+  {"Je m'appelle Marvin, je suis en train d'apprendre React et Next.js !"}
+</p>
+      </p>
+    </main>
+  )
+} 
---
+++ app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function Contact() {
+    return (
+      <main className="p-10">
+        <h1 className="text-3xl font-bold mb-4">Contact</h1>
+        <p className="text-gray-600">marvin.galeski@gmail.com</p>
+      </main>
+    )
+  }
